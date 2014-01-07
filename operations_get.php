57551788234b32d6ea9ab7b947769b6821c07b41
linkyndy <linkyndy@gmail.com>
2014-01-07 11:29:42 +0200

    Added PHP number operations scripts

---
+++ operations_get.php
@@ -0,0 +1,39 @@
+<!doctype html>
+
+<html>
+    <head>
+        <title>Operations</title>
+    </head>
+
+    <body>
+        <?php
+            if (isset($_GET['do'])) {
+                $a = $_GET['a'];
+                $b = $_GET['b'];
+
+                if (!is_numeric($a) || !is_numeric($b)) {
+                    echo "Numbers are not valid!";
+                } else {
+                    echo "Sum: " . ($a + $b) . "<br/>";
+                    echo "Difference: " . ($a - $b) . "<br/>";
+                    echo "Multiplication: " . ($a * $b) . "<br/>";
+                    if ($b != 0) {
+                        echo "Division: " . ($a / $b) . "<br/>";
+                    } else {
+                        echo "Division can not occur.<br/>";
+                    }
+                    if ($a != 0 || $b != 0) {
+                        echo "Power: " . pow($a, $b) . "<br/>";
+                    } else {
+                        echo "Power can not occur.<br/>";
+                    }
+                }
+            }
+        ?>
+        <form action="" method="get">
+            <input type="text" name="a" />
+            <input type="text" name="b" />
+            <input type="submit" name="do" value="Do operations!" />
+        </form>
+    </body>
+</html>
